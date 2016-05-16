c04c452d908c2df302ca263c1cccc6629c29598e
Kara Mansel <callmedevmaybe@gmail.com>
2016-05-16 19:34:11 -0400

    Adding blinking text, scrolling marquee, javascript

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-<img src="images/Grumpy-Cat.jpg" width="110" height="28" border="0" alt="javascript button">
+
+    function changeImage() {
+
+        if (document.getElementById("imgClickAndChange").src == "images/Greetings-from-the-Internet-Cats.png")
+        {
+            document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+        }
+        else
+        {
+            document.getElementById("imgClickAndChange").src = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQRwbWjYrX603ejYbsJzLWxrh3twn40kf8BKcDEm7kTgIWej05I";
+        }
+    }
+
+
+    function blinker() {
+    $('.blinking').fadeOut(500);
+    $('.blinking').fadeIn(500);
+    }
+    setInterval(blinker, 1000);
